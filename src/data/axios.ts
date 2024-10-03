527b46a7cacf18f4dbed187dcf81b0b02fd7bc33
kerexaaa <kerexakerexa@gmail.com>
2024-10-03 19:57:04 +0200

    setup https url for axios

--- src/data/axios.ts
+++ src/data/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { Weather, weatherSchema } from "./types";
 
 export const fetchData = async (city: string): Promise<Weather> => {
-  const url = `http://api.weatherapi.com/v1/current.json?key=40c3b4a710554f71865185229240110&q=${city}&aqi=no`;
+  const url = `https://api.weatherapi.com/v1/current.json?key=40c3b4a710554f71865185229240110&q=${city}&aqi=no`;
   const response = await axios.get<Weather>(url);
   const result = weatherSchema.safeParse(response.data);
   if (!result.success) {
